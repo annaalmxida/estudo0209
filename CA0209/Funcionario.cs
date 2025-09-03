e6493c770eeb27cb8a1cb806ce869269d7cd0ded
André Melchior <andre.melchior@outlook.com>
2025-09-03 17:24:36 -0300

    Funcionario.cs Pronto

--- CA0209/Funcionario.cs
+++ CA0209/Funcionario.cs
@@ -8,5 +8,23 @@ namespace CA0209
 {
     internal class Funcionario :Pessoa
     {
+        private int RegistroCarteira;
+        protected double Salario;
+        public string Cargo;
+        protected string Admissão;
+        protected double VA;
+        protected double VT;
+        protected double VR;
+        protected string AssistenciaMedica;
+
+        public void CadastrarFuncionario()
+        {
+            Console.WriteLine("Digite seu Registro:");
+            RegistroCarteira = int.Parse(Console.ReadLine());
+        }
+        public void ConsultarFuncionario()
+        {
+            Console.WriteLine("Olá, seu registro é: {0}", RegistroCarteira);
+        }
     }
 }
